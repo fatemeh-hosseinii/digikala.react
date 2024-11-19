ef7af2b1847f2e27fe1ea476258ee06155e6ab8b
fatemeh-hosseini1381 <1381fatemehhosseinii@gmail.com>
2024-11-19 23:21:27 +0330

    updating

--- src/Pages/Cart/DetailsCart.jsx
+++ src/Pages/Cart/DetailsCart.jsx
@@ -1,40 +1,83 @@
 import SpecialSell from "../../Media/SpecialSell.png"
 import {trashcan} from 'react-icons-kit/oct/trashcan'
 import {u2795} from 'react-icons-kit/noto_emoji_regular/u2795'
+import { ic_store_outline } from 'react-icons-kit/md/ic_store_outline';
+import { ic_verified_user_outline } from 'react-icons-kit/md/ic_verified_user_outline';
+import {androidCar} from 'react-icons-kit/ionicons/androidCar'
+import { ic_navigate_before_twotone } from 'react-icons-kit/md/ic_navigate_before_twotone';
+
 import Icon from "react-icons-kit";
 const DetailsCart = ({product}) => {
     return (
         <>
-            <div className="bg-gray-800">
-                <div className="bg-[yellow] mt-2 flex flex-row">
-                        <div className="w-[20%] bg-slate-500 flex flex-col p-">
-                            <div className="w-[70%]">
-                                <img src={product.image} alt="" />
-                            </div>
-                            <div className="mt-2 w-[60%] flex flex-row justify-center">
-                                <img src={SpecialSell} alt="" />
-                            </div>
-                            <div className="bg-[white] w-[60%] flex flex-row justify-between p-2 mt-3 rounded-lg border-[gray] border-2">
-                                <div>
-                                    <Icon icon={trashcan} className="text-[red]" />
+            <div className="bg-gray-800 flex flex-col">
+                <div className="bg-black p-2">
+                    <div className="bg-[yellow] mt-2 flex flex-row">
+                            <div className="w-[20%] bg-slate-500 flex flex-col p-">
+                                <div className="w-[90%]">
+                                    <img src={product.image} alt="" />
                                 </div>
-                                <div className="mt-1">
-                                    <p className="text-[red] font-bold">
-                                            ۱
-                                    </p>
+                                <div className="mt-2 w-[80%] flex flex-row justify-center">
+                                    <img className="w-[60%]" src={SpecialSell} alt="" />
                                 </div>
-                                <div>
-                                    <Icon icon={u2795} size={20} className="text-[red]"/>
-                                </div>
+                                
                             </div>
-                            
-                        </div>
-                        <div className="bg-[red] flex flex-col">
-                            <p className="text-[14px] font-bold">اسپیکر بلوتوثی قابل حمل ایکس-انرژی مدل Climber
-                            </p>
-                        </div>
-                </div>
+                            <div className="bg-[red] flex flex-col gap-2">
+                                <p className="text-[14px] font-bold">    
+                                اسپیکر بلوتوثی قابل حمل ایکس-انرژی مدل Climber
 
+                                </p>
+                                <p className="text-[12px]">مشکی</p>
+                                <div className="bg-[yellow] flex flex-row gap-2">
+                                    <Icon className="text-[#A1A3A8]" icon={ic_verified_user_outline} size={18}/>
+                                    <p className="text-[12px] mt-1">گارانتی 12 ماهه ipm</p>
+                                    
+                                </div>
+                                <div className="bg-[yellow] flex flex-row gap-2">
+                                    <Icon className="text-[#A1A3A8]" icon={ic_store_outline} size={18}/>
+                                    <p className="text-[12px] mt-1"> آرون گستر </p>
+                                    
+                                </div>
+                                <div className="bg-[yellow] flex flex-row gap-2">
+                                    <Icon className="text-[#EC4D6D]" icon={androidCar} size={18}/>
+                                    <p className="text-[12px] mt-1">  ارسال دیجی‌کالا </p>
+                                    
+                                </div>
+                                <div className="bg-[yellow] flex flex-row gap-2">
+                                    <Icon className="text-[#EC4D6D]" icon={androidCar} size={18}/>
+                                    <p className="text-[12px] mt-1">  ارسال امروز (فعلا در شهر تهران و کرج) </p>
+                                    
+                                </div>
+                            </div>
+                    </div>
+                   <div className="bg-[blue] flex flex-row p-2 gap-5">
+                        <div className="bg-[pink] w-[12%] rounded-md border-[gray] border-2 px-2 py-2 flex flex-row justify-between">
+                             <button>
+                                <Icon icon={trashcan} size={18} className="text-[red]"/>
+                            </button>  
+                            <p className="mt-1 text-[red]" >1</p>
+                            <button>
+                                <Icon icon={u2795} size={18} className="text-[red]"/>
+                            </button>                       
+                        </div>
+                        <div className="flex flex-col">
+                            <div className="flex flex-row gap-2">
+                                <p className="text-[11px] text-[red]">۹۲۰,۰۰۰ تومان</p>
+                                <span className="text-[11px] text-[red]">تخفیف</span>
+                                
+                            </div>
+                            <p>۲,۴۷۹,۰۰۰ تومان</p>
+                        </div>
+                   </div>
+                </div>
+               <div className="bg-rose-900 flex flex-row justify-end">
+                <div className="flex flex-row">
+                    <p className="text-[13px] text-[#3EBFD6] mt-1">انتقال به خرید بعدی</p>
+                    <Icon className="text-[#3EBFD6]" icon={ic_navigate_before_twotone} size={22} />
+                </div>
+                       
+               </div>
+              
             </div>
         </>
       );
--- src/Pages/Productpackaging/Productpackaging.jsx
+++ src/Pages/Productpackaging/Productpackaging.jsx
@@ -1,38 +1,88 @@
-
 import { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { fetchpost_eight } from "../../Redux/ProductpackagSlice";
+import { Swiper, SwiperSlide } from 'swiper/react';
+import 'swiper/css';
+import 'swiper/css/navigation';
+import { Navigation } from "swiper/modules";
 
 const Productpakaging = () => {
-    const{productpackage,error,loading}=useSelector(state=>state.Productpackag)
-    const dispatch=useDispatch()
-    useEffect(()=>{
-        dispatch(fetchpost_eight())
-    },[])
-    return (  <>
-    <div className=" p-3">
-        <div className=" flex flex-col justify-center items-center">
-            <h2 className="text-center text-[19px]">خرید بر اساس دسته‌بندی</h2>
-            <div className=" mt-5 gap-14 w-[90%] p-5 flex flex-row flex-wrap justify-around">
-                {
-                    productpackage?.map((elem)=>{
-                        return(
-                            <div className="flex flex-col w-[8%] items-center ">
-                                <img src={elem.image} />
-                   
-                                <p className="text-[12px] text-center rounded-full">{elem.title}</p>
-                    
-                             </div>
-                        )
-                    })
-                }
-                
-                
-            </div>
-        </div>
-        
+  const { productpackage, error, loading } = useSelector((state) => state.Productpackag);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(fetchpost_eight());
+  }, [dispatch]);
+
+  if (loading) return <div>Loading...</div>;
+  if (error) return <div>Error loading product packages: {error.message}</div>;
+
+  const groupedItems = [];
+  for (let i = 0; i < productpackage?.length; i += 2) {
+    groupedItems.push(productpackage.slice(i, i + 2));
+  }
+
+  return (
+    <div className="p-3">
+      <div className="flex flex-col justify-center items-center xl:container p-2 ">
+        <h2 className="text-center text-[19px] font-bold">خرید بر اساس دسته‌بندی</h2>
+        <Swiper
+          modules={[Navigation]}
+          breakpoints={{
+            1200:{
+                slidesPerView:8
+            },
+            680:{
+                slidesPerView:6.5
+            },
+            580:{
+                slidesPerView:5.5
+            },
+            480:{
+                slidesPerView:4.5
+            },
+            380: {
+              slidesPerView: 3.5,
+            },
+          }}
+          spaceBetween={20}
+          navigation
+          className="mySwiper mt-[20px] h-[100%] w-[100%]"
+        >
+          {groupedItems?.map((group, index) => (
+            <SwiperSlide key={index} className=" flex flex-col  justify-center items-center  p-2 h-full">
+              {group.map((elem) => (
+               <div className="flex flex-col lg:w-[70%] w-[100%] gap-2 mt-5 items-center">
+                    <img src={elem.image} />
+                                  
+                    <p className="lg:text-[12px] md:text-[11px] text-[10px] text-center font-bold">{elem.title}</p>
+                                   
+                </div>
+              ))}
+              
+            </SwiperSlide>
+          ))}
+        </Swiper>
+      </div>
     </div>
-    </>);
-}
- 
+  );
+};
+
 export default Productpakaging;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
